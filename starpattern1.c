5e0e56890c0e7d2c34fdbc3027147e98a9f28ace
SAMYUKTHA <38600655+samyukthagopalsamy@users.noreply.github.com>
2019-03-09 23:57:56 +0530

    Add files via upload

---
+++ starpattern1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+  int i,j,rows;
+  printf("Enter the number of rows");
+  scanf("%d",&rows);
+
+  for(i=1;i<=rows;i++)
+  {
+      for(j=1;j<=i;j++)
+      {
+          printf("*");
+
+      }
+      printf("\n");
+  }
+}
---
+++ starpattern2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    for(i=1;i<=5;i++)
+    {
+        for(j=i;j<5;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<(i*2);k++)
+        {
+                printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
