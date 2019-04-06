d7e8e48f7fbf031c35e0942b0cd230c73b4aa144
SAMYUKTHA <38600655+samyukthagopalsamy@users.noreply.github.com>
2019-04-06 20:43:58 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
+
 int main()
-{int i ,j,k;
-k=1;
-for (i=1;i<5;i++)
 {
-  for(j=1;j<=i;j++)
-  {
-  printf("  %d",k++);
-  }
-printf("\n");
+    
+    int i ,j,k,n;
+    k=1;
+    printf("Number of rows required : ");
+    scanf("%d",&n);
+        for (i=0;i<n;i++)//loops till number of rows
+            {
+                for(j=0;j<=i;j++) // prints stars equal to row number
+                    printf("  *",k++);
+                printf("\n");
+             }
 
+    return 0;
 }
 
-return 0;
 
+/*
+output:
+*
+* *
+* * *
+* * * *
+*/
 
-}
